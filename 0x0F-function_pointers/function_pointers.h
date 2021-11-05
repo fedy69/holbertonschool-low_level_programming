8157dcc4da05554d28b4bbb3216ddddd3621b268
Fedy chihaoui <84166701+fedy69@users.noreply.github.com>
2021-11-05 23:20:15 +0100

    FIX

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,8 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
-#include <stdio.h>
 int _putchar(char c);
-void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif
