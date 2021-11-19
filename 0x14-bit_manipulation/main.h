affbe8cd408bf40585d09d07371a6dce51f27a35
Fedy chihaoui <84166701+fedy69@users.noreply.github.com>
2021-11-19 21:53:24 +0100

    main

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,8 @@
 #ifndef MAIN_H
-#define  MAIN_H
+#define MAIN_H
 
-
+#include <stdio.h>
+#include <stdlib.h>
 
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
@@ -9,6 +10,7 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
-
+int get_endianness(void);
+int _putchar(char c);
 
 #endif
